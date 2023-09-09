e776187fd9bc1e099498d29db158a9ecdead38b9
joewrotehaikus <78707288+joewrotehaikus@users.noreply.github.com>
2023-09-09 16:50:48 -0400

    Change stream listeners loop through clients Rather than have each connected web socket client with own change stream listener. This is because the former method caused too many event listeners on the server. So, an array of connected clients was made, and every time a client connects, they are pushed. Every time a connection closes, the client is spliced out.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import http from "http";
 import connectDB from "./db.js";
 connectDB();
 import { Source, Note, Quiz, User, Group } from "./models/index.js";
+import { canRead, canEdit } from "./lib/auth.js";
 
 const httpServer = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "application/json" });
@@ -13,23 +14,25 @@ const sourceChange = Source.watch();
 const noteChange = Note.watch();
 const quizChange = Quiz.watch();
 
+const clients = [];
+
 const server = new WebSocketServer({ server: httpServer });
 
 server.on("connection", async (ws, req) => {
-  console.log("A client connected", req.socket.remoteAddress);
+  clients.push(ws);
+  console.log("connected", clients);
   const cookies = req.headers.cookie;
-  console.log(cookies);
   let token = cookies
     ?.split("; ")
     .find((cookie) => /token=/.test(cookie))
     ?.split("=")[1];
-  console.log(token);
   if (token) {
     const user = await User.findOne({ refreshTokens: token });
     console.log(user.username);
+    ws.user = user;
     ws.send(JSON.stringify({ message: `Welcome, ${user.username}!` }));
   } else {
-    ws.send(JSON.stringify({ message: "Welcome!"}))
+    ws.send(JSON.stringify({ message: "Welcome!" }));
   }
 
   ws.on("message", (data) => {
@@ -37,17 +40,55 @@ server.on("connection", async (ws, req) => {
   });
 
   ws.on("close", () => {
+    const index = clients.indexOf(ws);
+    if (index > -1) {
+      clients.splice(index, 1);
+    }
+
     console.log("Connection closed");
+    console.log(clients);
   });
+});
 
-  sourceChange.on("change", (change) => {
-    console.log(change);
-    ws.send(JSON.stringify(change));
+sourceChange.on("change", (change) => {
+  console.log(change);
+  const source = change.fullDocument;
+  clients.forEach((client) => {
+    if (canRead(source, client.user)) {
+      client.send(JSON.stringify(change));
+    } else {
+      console.log(
+        `User ${client.user?.username} is not authorized to view source title "${source.title}"`
+      );
+    }
   });
+});
 
-  noteChange.on("change", (change) => {
-    console.log(change);
-    ws.send(JSON.stringify(change));
+noteChange.on("change", (change) => {
+  console.log(change);
+  const note = change.fullDocument;
+  clients.forEach((client) => {
+    if (canRead(note, client.user)) {
+      client.send(JSON.stringify(change));
+    } else {
+      console.log(
+        `User ${client.user?.username} is not authorized to view note "${note.text}"`
+      );
+    }
+  });
+});
+
+quizChange.on("change", (change) => {
+  console.log(change);
+  const quiz = change.fullDocument;
+  clients.forEach((client) => {
+    if (canRead(quiz, client.user)) {
+      client.send(JSON.stringify(change));
+    } else {
+      console.log(
+        `User ${client.user.username} is not authorized to view quiz "${quiz.prompt}"`
+      );
+    }
   });
 });
 
